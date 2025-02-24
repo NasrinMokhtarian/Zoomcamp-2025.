b7ca4bc546de26ff1bcf2df345e4124f46d98967
Nasrin Mokhtarian <nasrin.mokhtarian@gmail.com>
2025-02-24 19:06:50 +0000

    finalizing

---
+++ dbtBigQueryproject/ny_taxi_trips/models/staging/stg_yellow_trips.sql
@@ -0,0 +1,50 @@
+{{ config(materialized='view') }}
+ 
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
+  from {{ source('staging','yellow_trips') }}
+  where vendorid is not null 
+)
+select
+   -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,    
+    {{ dbt.safe_cast("vendorid", ("integer")) }} as vendorid,
+    {{ dbt.safe_cast("ratecodeid", ("integer")) }} as ratecodeid,
+    {{ dbt.safe_cast("pulocationid", ("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("dolocationid", ("integer")) }} as dropoff_locationid,
+
+    -- timestamps
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    store_and_fwd_flag,
+    {{ dbt.safe_cast("passenger_count",("integer")) }} as passenger_count,
+    cast(trip_distance as numeric) as trip_distance,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
+    
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(0 as numeric) as ehail_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    coalesce({{ dbt.safe_cast("payment_type", ("integer")) }},0) as payment_type,
+    {{ get_payment_type_description('payment_type') }} as payment_type_description,
+    cast(0 as numeric) as congestion_surcharge
+
+from tripdata
+where rn = 1
+
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
